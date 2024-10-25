b69c20b8c98e01e2a6ea3bf2be4247448f08dcdf
Marcus401 <mjmmacaba@gmail.com>
2024-10-25 21:16:42 +0800

    routes

--- SupplyHub.Server/obj/Debug/net8.0/SupplyHub.Server.AssemblyInfo.cs
+++ SupplyHub.Server/obj/Debug/net8.0/SupplyHub.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SupplyHub.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46277b5927fee676c1f7f5adbbd40580710ae9cb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cce81b3074e172d2c82b996279107c0bb1d8e05")]
 [assembly: System.Reflection.AssemblyProductAttribute("SupplyHub.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SupplyHub.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
