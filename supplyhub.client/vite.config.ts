4b761c62e61f019eb8eabe8d89149f4624e41a06
Marcus401 <mjmmacaba@gmail.com>
2024-10-25 17:33:13 +0800

    cert

--- supplyhub.client/vite.config.ts
+++ supplyhub.client/vite.config.ts
@@ -1,20 +1,13 @@
 import { fileURLToPath, URL } from 'node:url';
-
 import { defineConfig } from 'vite';
 import plugin from '@vitejs/plugin-react';
 import fs from 'fs';
-import path from 'path';
 import child_process from 'child_process';
 import { env } from 'process';
 
-const baseFolder =
-    env.APPDATA !== undefined && env.APPDATA !== ''
-        ? `${env.APPDATA}/ASP.NET/https`
-        : `${env.HOME}/.aspnet/https`;
-
 const certificateName = "supplyhub.client";
-const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
-const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
+const certFilePath = `https/${certificateName}.pem`
+const keyFilePath = `https/${certificateName}.key`
 
 if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
     if (0 !== child_process.spawnSync('dotnet', [
