a006565cce6f2c226af7379632449b32bf796f8d
Marcus401 <mjmmacaba@gmail.com>
2024-10-25 13:41:26 +0800

    sellerDashboard

--- supplyhub.client/src/Pages/Seller/Seller.tsx
+++ supplyhub.client/src/Pages/Seller/Seller.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import SellerDashboard from "../../Components/SellerPagesComponents/SellerDashboard/SellerDashboard.tsx";
 
 type Props = {};
 
 const Seller = (props: Props) => {
     return (
-        <div>Seller</div>
+        <>
+            <SellerDashboard>
+
+            </SellerDashboard>
+        </>
     );
 };
 
