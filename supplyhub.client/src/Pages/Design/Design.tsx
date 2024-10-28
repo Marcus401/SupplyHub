7e02c5605ff8e5d31f066142cbdfb39fd084e4fb
Marcus401 <mjmmacaba@gmail.com>
2024-10-28 14:10:25 +0800

    added react library

--- supplyhub.client/src/Pages/Design/Design.tsx
+++ supplyhub.client/src/Pages/Design/Design.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import SellerProductCard from "../../Components/SellerPagesComponents/SellerProductCard/SellerProductCard.tsx";
 
 type Props = {};
 
 const Design = (props: Props) => {
     return (
-        <div>Design</div>
+        <div>
+            <div>
+                <h6>Seller Product Card</h6>
+                <SellerProductCard/>
+            </div>
+        </div>
     );
 };
 
