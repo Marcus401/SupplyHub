ff6df3e193503d9c5d1f29136d0d533ac7950cfb
Marcus401 <mjmmacaba@gmail.com>
2024-10-29 21:53:43 +0800

    column fix

--- supplyhub.client/src/App.tsx
+++ supplyhub.client/src/App.tsx
@@ -3,15 +3,10 @@ import {Outlet} from "react-router";
 function App() {
     return (
         <div className="flex justify-center w-full">
-            {/* Left Column */}
             <div className="hidden 2xl:block w-0 2xl:w-[calc(100vw - 768px)]"></div>
-
-            {/* Middle Column */}
             <div className="relative flex-grow max-w-[1536px]">
                 <Outlet />
             </div>
-
-            {/* Right Column */}
             <div className="hidden 2xl:block w-0 2xl:w-[calc(100vw - 768px)]"></div>
         </div>
     );
