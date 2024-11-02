4cff8f80942af1b60b6334e7077cdce2a2f85b42
Glenn Batac <glennpascual66@gmail.com>
2024-11-02 21:12:57 +0000

    SellerReviewList

--- supplyhub.client/src/Components/SellerPagesComponents/SellerReviewsList/SellerReviewsList.tsx
+++ supplyhub.client/src/Components/SellerPagesComponents/SellerReviewsList/SellerReviewsList.tsx
@@ -1,14 +1,53 @@
-import React, {useEffect} from 'react'
+import React, { useEffect } from 'react';
+import ReviewCard from '../SellerReviewCard/SellerReviewCard';
+//mock reviews; to be changed
+const reviews = [
+  {
+    id: 1,
+    userName: 'User Name',
+    affiliatedCompany: 'Affiliated Company',
+    rating: 4,
+    comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    profilePictureUrl: 'https://avatar.iran.liara.run/public/boy?username=Ash',
+  },
+  {
+    id: 2,
+    userName: 'User Name',
+    affiliatedCompany: 'Affiliated Company',
+    rating: 5,
+    comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    profilePictureUrl: 'https://avatar.iran.liara.run/public/boy?username=Ash',
+  },
 
-type Props = {};
 
-const SellerReviewsList = (props: Props) => {
-    useEffect(() => {
-        document.title = "Reviews: Seller";
-    }, []);
+  // Add more review objects as needed
+];
 
-    return (
-        <div>SellerReviews</div>
-    );
+const ReviewCardList: React.FC = () => {
+  useEffect(() => {
+    document.title = 'Reviews';
+  }, []);
+
+  return (
+    <div className="flex flex-col w-full p-6">
+      <h2 className="text-2xl font-bold mb-4">Reviews</h2>
+      <div className="space-y-4">
+        {reviews.map((review) => (
+          <ReviewCard
+            key={review.id}
+            userName={review.userName}
+            affiliatedCompany={review.affiliatedCompany}
+            rating={review.rating}
+            comment={review.comment}
+            profilePictureUrl={review.profilePictureUrl}
+          />
+
+          
+        ))}
+        
+      </div>
+    </div>
+  );
 };
-export default SellerReviewsList;
+
+export default ReviewCardList;
