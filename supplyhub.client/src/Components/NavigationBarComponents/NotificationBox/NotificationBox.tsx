4ef04dde8fb432490c878a204f66f6be7d66578f
Marcus401 <mjmmacaba@gmail.com>
2024-11-06 22:03:07 +0800

    added notification box component

---
+++ supplyhub.client/src/Components/NavigationBarComponents/NotificationBox/NotificationBox.tsx
@@ -0,0 +1,11 @@
+﻿import React from 'react'
+
+type Props = {};
+
+const NotificationBox = (props: Props) => {
+    return (
+        <div>NotificationBox</div>
+    );
+};
+
+export default NotificationBox;
