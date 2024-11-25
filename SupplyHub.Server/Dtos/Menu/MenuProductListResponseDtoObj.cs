4c438121981d6c8d0c219a8573f8a922f2dd7cba
Dawn995 <genielferrer402@gmail.com>
2024-11-26 04:40:52 +0800

    Changed the data type of 'Price' property from 'int' to 'decimal'

--- SupplyHub.Server/Dtos/Menu/MenuProductListResponseDtoObj.cs
+++ SupplyHub.Server/Dtos/Menu/MenuProductListResponseDtoObj.cs
@@ -4,7 +4,7 @@ public class MenuProductListResponseDtoObj
 {
 	public byte[]? Thumbnail { get; set; }
 	public required int ProductId { get; set; }
-	public required int Price { get; set; }
+	public required decimal Price { get; set; }
 	public required string ProductName { get; set; }
 	public string? Unit { get; set; }
 }
