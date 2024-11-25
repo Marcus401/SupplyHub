49f03766bae76c02b48cc0a2d4790f6488347557
ricaarroyo <ricasofiarroyo@gmail.com>
2024-11-25 13:49:11 +0800

    added review button to usersellerprofileinfo

--- supplyhub.client/src/Components/ProfileComponents/UserSellerProfileInfo/UserSellerProfileInfo.tsx
+++ supplyhub.client/src/Components/ProfileComponents/UserSellerProfileInfo/UserSellerProfileInfo.tsx
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom";
 import { VscArrowLeft } from "react-icons/vsc";
 import coverPic from "../../../assets/background.png";
 import profilePic from "../../../assets/default-profile-picture-avatar-photo-placeholder-vector-illustration-default-profile-picture-avatar-photo-placeholder-vector-189495158.webp";
-import SellerReviewsList from "../../SellerPagesComponents/SellerReviewsList/SellerReviewsList";
-type Props = {};
+import ProductReviewCardList from "../../ProductComponents/ProductReviewCardList/ProductReviewCardList";
 
-const UserSellerProfileInfo = (props: Props) => {
+const UserSellerProfileInfo = () => {
   return (
-    <div className="max-w-[1100px] mx-auto p-4">
+    <div className="max-w-[1100px] items-center mx-auto p-4">
+      {/* Cover photo and back button */}
       <div className="relative flex h-[50px] overflow-hidden items-center">
         <img
           src="https://wallpaperaccess.com/full/1560881.png"
@@ -21,11 +21,12 @@ const UserSellerProfileInfo = (props: Props) => {
           <VscArrowLeft className="text-white w-5 h-5" />
         </Link>
       </div>
+
+      {/* Profile section */}
       <div className="relative w-full h-[300px] items-center overflow-hidden mx-auto">
         <img src={coverPic} className="object-cover w-full h-full" />
       </div>
-
-      <div className="relative bottom-[57px] left-0 overflow-visible items-center ">
+      <div className="relative bottom-[57px] left-0 overflow-visible items-center">
         <img
           src={profilePic}
           className="object-cover rounded-full w-32 h-32 border-4 border-white shadow-lg"
@@ -33,20 +34,22 @@ const UserSellerProfileInfo = (props: Props) => {
         />
       </div>
 
-      <div className="flex items-center justify-between max-w-[1050px] pl-[140px] -mt-[120px]">
+      <div className="flex items-center justify-between max-w-[500px] pl-[140px] -mt-[120px]">
         <div className="text-left">
-          <h2 className="mb-10 text-2xl font-bold">Name</h2>
+          <h2 className="mb-10 text-4xl font-bold">Name</h2>
         </div>
       </div>
-      <div className="min-h-[100px] max-h-[500px] w-full max-w-[500px] p-4 mt-4 border border-gray-100 rounded-lg">
+
+      {/* User description */}
+      <div className="min-h-[100px] max-h-[500px] w-full max-w-[500px] p-4 mt-4 mb-4 border border-gray-200 rounded-lg">
         <p className="text-black text-base">
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas, eaque
           odio atque culpa voluptas sequi neque sint placeat vero tempore!
         </p>
       </div>
 
-      <div className="w-[610px] ml-[510px] flex-row -mt-[150px]">
-        <SellerReviewsList />
+      <div className="w-full mt-8">
+        <ProductReviewCardList />
       </div>
     </div>
   );
