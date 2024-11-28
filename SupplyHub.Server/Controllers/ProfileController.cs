3e454b5a664b8636b3d23c02ba83de15b574c3c5
Dawn995 <genielferrer402@gmail.com>
2024-11-29 04:09:29 +0800

    Added private methods to handle null properties passed to 'EditProfile'

--- SupplyHub.Server/Controllers/ProfileController.cs
+++ SupplyHub.Server/Controllers/ProfileController.cs
@@ -91,19 +91,14 @@ public class ProfileController(SupplyhubDbContext context, UserManager<User> use
 		var userRoles = await _userManager.GetRolesAsync(userProfile);
 		var userRole = userRoles.First();
 
-		userProfile.UserName = editUserProfileRequestDto.UserName;
-		userProfile.Bio = editUserProfileRequestDto.Bio;
-		userProfile.ProfilePicture = editUserProfileRequestDto.ProfilePicture;
-		userProfile.CoverPicture = editUserProfileRequestDto.CoverPicture;
+		UpdateUserProfile(userProfile, editUserProfileRequestDto);
 		if (userRole == "Seller")
 		{
 			var sellerAdditionalInfo = JsonSerializer.Deserialize<Dtos.Profile.SellerInfo>(editUserProfileRequestDto.AdditionalInfo.ToString());
 			if (sellerAdditionalInfo != null)
 			{
 				var sellerInfo = await _context.SellerInfos.Include(u => u.User).FirstOrDefaultAsync(u => u.UserId == intUserId);
-				sellerInfo!.Socials = sellerAdditionalInfo.Socials;
-				sellerInfo.BusinessType = sellerAdditionalInfo.BusinessType;
-				sellerInfo.Location = sellerAdditionalInfo.Location;
+				UpdateSellerInfo(sellerInfo!, sellerAdditionalInfo);
 			}
 		}
 		else if (userRole == "User")
@@ -112,13 +107,66 @@ public class ProfileController(SupplyhubDbContext context, UserManager<User> use
 			if (userAdditionalInfo != null)
 			{
 				var userInfo = await _context.UserInfos.Include(u => u.User).FirstOrDefaultAsync(u => u.UserId == intUserId);
-				userInfo!.Position = userAdditionalInfo.Position;
-				userInfo.CompanyUserId = userAdditionalInfo.CompanyUserId;
+				UpdateUserInfo(userInfo!, userAdditionalInfo);
 			}
 		}
 
 		await _context.SaveChangesAsync();
 
 		return Ok();
+	}
+
+	private void UpdateUserProfile(User user, EditUserProfileRequestDto editUserProfileRequestDto)
+	{
+		if (editUserProfileRequestDto.UserName != null)
+		{
+			user.UserName = editUserProfileRequestDto.UserName;
+		}
+
+		if (editUserProfileRequestDto.Bio != null)
+		{
+			user.Bio = editUserProfileRequestDto.Bio;
+		}
+
+		if (editUserProfileRequestDto.ProfilePicture != null)
+		{
+			user.ProfilePicture = editUserProfileRequestDto.ProfilePicture;
+		}
+
+		if (editUserProfileRequestDto.CoverPicture != null)
+		{
+			user.CoverPicture = editUserProfileRequestDto.CoverPicture;
+		}
+	}
+
+	private void UpdateSellerInfo(Models.SellerInfo seller, Dtos.Profile.SellerInfo additionalInfo)
+	{
+		if (additionalInfo.Socials != null)
+		{
+			seller.Socials = additionalInfo.Socials;
+		}
+
+		if (additionalInfo.BusinessType != null)
+		{
+			seller.BusinessType = additionalInfo.BusinessType;
+		}
+
+		if (additionalInfo.Location != null)
+		{
+			seller.Location = additionalInfo.Location;
+		}
+	}
+
+	private void UpdateUserInfo(Models.UserInfo user, Dtos.Profile.UserInfo additionalInfo)
+	{
+		if (additionalInfo.Position != null)
+		{
+			user.Position = additionalInfo.Position;
+		}
+
+		if (additionalInfo.CompanyUserId != null)
+		{
+			user.CompanyUserId = additionalInfo.CompanyUserId;
+		}
 	}
 }
